6f33671709bf346a3e2ed317c8c345a4204c854c
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-08-25 16:43:40 +0200

    chore(config): integrate next-pwa into next.config.mjs

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 
+import withPWAInit from "@ducanh2912/next-pwa";
+
+/** @type {import('next').NextConfig} */ 
+
+const withPWA = withPWAInit({
+  dest: 'public'
+})
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ['m.media-amazon.com']
+  },
 };
 
-export default nextConfig;
+export default withPWA({ ...nextConfig});
